20570e5ddfc4af340665023ebf3f2eb7a1e208f5
Paul Copplestone <pcopplestone@gmail.com>
2020-10-12 21:30:07 +0800

    Converts all to Typescript

--- src/constants.ts
+++ src/constants.ts
@@ -1,27 +1,32 @@
 export const VSN: string = '1.0.0'
 
-export const SOCKET_STATES = { connecting: 0, open: 1, closing: 2, closed: 3 }
-
 export const DEFAULT_TIMEOUT = 10000
 
 export const WS_CLOSE_NORMAL = 1000
 
-export const CHANNEL_STATES = {
-  closed: 'closed',
-  errored: 'errored',
-  joined: 'joined',
-  joining: 'joining',
-  leaving: 'leaving',
+export enum SOCKET_STATES {
+  connecting = 0,
+  open = 1,
+  closing = 2,
+  closed = 3,
 }
 
-export const CHANNEL_EVENTS = {
-  close: 'phx_close',
-  error: 'phx_error',
-  join: 'phx_join',
-  reply: 'phx_reply',
-  leave: 'phx_leave',
+export enum CHANNEL_STATES {
+  closed = 'closed',
+  errored = 'errored',
+  joined = 'joined',
+  joining = 'joining',
+  leaving = 'leaving',
 }
 
-export const TRANSPORTS = {
-  websocket: 'websocket',
+export enum CHANNEL_EVENTS {
+  close = 'phx_close',
+  error = 'phx_error',
+  join = 'phx_join',
+  reply = 'phx_reply',
+  leave = 'phx_leave',
+}
+
+export enum TRANSPORTS {
+  websocket = 'websocket',
 }
---
+++ src/push.ts
@@ -0,0 +1,112 @@
+import { DEFAULT_TIMEOUT, CHANNEL_EVENTS } from './constants'
+import Channel from './channel'
+
+export default class Push {
+  sent: boolean = false
+  timeoutTimer: number | undefined = undefined
+  ref: string = ''
+  receivedResp: {
+    status: string
+    response: Function
+  } | null = null
+  recHooks: {
+    status: string
+    callback: Function
+  }[] = []
+  refEvent: string | null = null
+
+  /**
+   * Initializes the Push
+   *
+   * @param channel The Channel
+   * @param event The event, for example `"phx_join"`
+   * @param payload The payload, for example `{user_id: 123}`
+   * @param timeout The push timeout in milliseconds
+   */
+  constructor(
+    public channel: Channel,
+    public event: string,
+    public payload: any = {},
+    public timeout: number = DEFAULT_TIMEOUT
+  ) {}
+
+  resend(timeout: number) {
+    this.timeout = timeout
+    this.cancelRefEvent()
+    this.ref = ''
+    this.refEvent = null
+    this.receivedResp = null
+    this.sent = false
+    this.send()
+  }
+
+  send() {
+    if (this.hasReceived('timeout')) {
+      return
+    }
+    this.startTimeout()
+    this.sent = true
+    this.channel.socket.push({
+      topic: this.channel.topic,
+      event: this.event,
+      payload: this.payload,
+      ref: this.ref,
+    })
+  }
+
+  receive(status: string, callback: Function) {
+    if (this.hasReceived(status)) {
+      callback(this.receivedResp?.response)
+    }
+
+    this.recHooks.push({ status, callback })
+    return this
+  }
+
+  // private
+
+  matchReceive({ status, response }: { status: string; response: Function }) {
+    this.recHooks
+      .filter((h) => h.status === status)
+      .forEach((h) => h.callback(response))
+  }
+
+  cancelRefEvent() {
+    if (!this.refEvent) {
+      return
+    }
+    this.channel.off(this.refEvent)
+  }
+
+  cancelTimeout() {
+    clearTimeout(this.timeoutTimer)
+    this.timeoutTimer = undefined
+  }
+
+  startTimeout() {
+    if (this.timeoutTimer) {
+      return
+    }
+    this.ref = this.channel.socket.makeRef()
+    this.refEvent = this.channel.replyEventName(this.ref)
+
+    this.channel.on(this.refEvent, (payload: any) => {
+      this.cancelRefEvent()
+      this.cancelTimeout()
+      this.receivedResp = payload
+      this.matchReceive(payload)
+    })
+
+    this.timeoutTimer = <any>setTimeout(() => {
+      this.trigger('timeout', {})
+    }, this.timeout)
+  }
+
+  hasReceived(status: string) {
+    return this.receivedResp && this.receivedResp.status === status
+  }
+
+  trigger(status: string, response: any) {
+    if (this.refEvent) this.channel.trigger(this.refEvent, { status, response })
+  }
+}
