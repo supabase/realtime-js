a20dc9750a1778481ed8f430845a8878dcce1922
Paul Copplestone <pcopplestone@gmail.com>
2020-10-13 18:10:01 +0800

    chore: Adds a basic example and notes in the Readme

--- README.md
+++ README.md
@@ -1,43 +1,78 @@
 # Realtime Client
 
-Listens to changes in a PostgreSQL Database and broadcasts them over websockets.
+Listens to changes in a PostgreSQL Database and via websockets.
 
-# Usage
+## Usage
 
-This is for usage with Supabase Realtime server.
+This is for usage with Supabase [Realtime](https://github.com/supabase/realtime) server.
 
-Basic usage:
+#### Creating a Socket connection
+
+You can set up one connection to be used across the whole app.
 
 ```js
 import { Socket } from '@supabase/realtime-js'
 
 var socket = new Socket(process.env.REALTIME_URL)
 socket.connect()
-
-// Listen to only INSERTS on the 'users' table in the 'public' schema
-var allChanges = this.socket.channel('realtime:public:users')
-  .on('INSERT', payload => { console.log('Update received!', payload) })
-  .subscribe()
-
-// Listen to all changes from the 'public' schema
-var allChanges = this.socket.channel('realtime:public')
-  .on('*', payload => { console.log('Update received!', payload) })
-  .subscribe()
-
-// Listen to all changes in the database
-let allChanges = this.socket.channel('realtime:*')
-  .on('*', payload => { console.log('Update received!', payload) })
-  .subscribe()
 ```
 
-See full instructions this repository: [Supabase Realtime](https://github.com/supabase/realtime).
+**Socket events**
 
-# Credits
+```js
+socket.onOpen(() => console.log('Socket opened.'))
+socket.onClose(() => console.log('Socket closed.'))
+socket.onError((e) => console.log('Socket error', e.message))
+```
+
+#### Subscribing to events
+
+You can listen to `INSERT`, `UPDATE`, `DELETE`, or all `*` events.
+
+You can subscribe to events:
+
+- For the whole database: `realtime`
+- For a particular schema: `realtime:{SCHEMA}`. eg: `realtime:public`
+- For a particular table: `realtime:{SCHEMA}:{TABLE}`. eg: `realtime:public:users`
+- For individual columns: `realtime:{SCHEMA}:{TABLE}:{COL}.eq.{VAL}`. eg: `realtime:public:users:id.eq.1`
+
+
+```js
+var publicSchema = socket.channel('realtime:public')
+publicSchema.on('*', (e) => console.log(e))
+publicSchema.on('INSERT', (e) => console.log(e))
+publicSchema.on('UPDATE', (e) => console.log(e))
+publicSchema.on('DELETE', (e) => console.log(e))
+publicSchema.subscribe()
+
+var usersTable = socket.channel('realtime:public:users')
+usersTable.on('*', (e) => console.log(e))
+usersTable.on('INSERT', (e) => console.log(e))
+usersTable.on('UPDATE', (e) => console.log(e))
+usersTable.on('DELETE', (e) => console.log(e))
+usersTable.subscribe()
+```
+
+**Subscription events**
+
+```js
+
+publicSchema
+  .subscribe()
+  .receive('ok', () => console.log('Connected.'))
+  .receive('error', () => console.log('Failed.'))
+  .receive('timeout', () => console.log('Timed out, retrying.'))
+
+```
+
+
+
+## Credits
 
 - Original Node.js client was made by Mario Campa of [phoenix-channels](github.com/mcampa/phoenix-client).
 - API was made by authors of the [Phoenix Framework](http://www.phoenixframework.org/). See their website for complete list of authors.
 
-# License
+## License
 
 MIT. License is the same as [phoenix-channels](https://github.com/mcampa/phoenix-client) and [Phoenix Framework](https://phoenixframework.org/).
 
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,36 +1,79 @@
-
-import { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_KEY } from '../lib/constants'
+import { useState } from 'react'
+import {
+  NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_KEY,
+} from '../lib/constants'
 
 import { Socket } from '@supabase/realtime-js'
 
 var socket = new Socket(NEXT_PUBLIC_SUPABASE_URL, {
   params: { apikey: NEXT_PUBLIC_SUPABASE_KEY },
 })
+socket.connect()
 
 socket.onOpen(() => console.log('Socket opened.'))
 socket.onClose(() => console.log('Socket closed.'))
-socket.onError(e => console.log('Socket error', e.m))
-
-// Listen to only INSERTS on the 'users' table in the 'public' schema
-var userInserts = socket
-  .channel('realtime:public:users')
-  .subscribe()
-  // .on('INSERT', (payload) => {
-  //   console.log('Update received!', payload)
-  // })
-
-// // Listen to all changes from the 'public' schema
-// var allChanges = socket
-//   .channel('realtime:public')
-//   .join()
-//   .on('*', (payload) => {
-//     console.log('Update received!', payload)
-//   })
-
+socket.onError((e) => console.log('Socket error', e.message))
 
 export default function IndexPage() {
-  socket.connect()
-  // console.log('userInserts', userInserts)
+  let [inserts, setInserts] = useState([])
+  let [updates, setUpdates] = useState([])
+  let [deletes, setDeletes] = useState([])
 
-  return <div className="w-full h-full bg-gray-300"></div>
+  var publicSchema = socket.channel('realtime:public')
+  publicSchema.on('INSERT', (e) => setInserts([...inserts, e]))
+  publicSchema.on('UPDATE', (e) => setUpdates([...updates, e]))
+  publicSchema.on('DELETE', (e) => setDeletes([...deletes, e]))
+  publicSchema
+    .subscribe()
+    .receive('ok', () => console.log('Connected.'))
+    .receive('error', () => console.log('Failed.'))
+    .receive('timeout', () => console.log('Timed out, retrying.'))
+
+  return (
+    <div className="w-full h-full flex">
+      <div className="col w-1/3">
+        <div>
+          <h3 className="font-mono">INSERTS</h3>
+          {inserts.map((x) => (
+            <pre
+              key={x.commit_timestamp}
+              className="text-xs overflow-scroll border border-black rounded-md m-2 p-2"
+              style={{ maxHeight: 200 }}
+            >
+              {JSON.stringify(x, null, 2)}
+            </pre>
+          ))}
+        </div>
+      </div>
+      <div className="col w-1/3">
+        <div>
+          <h3 className="font-mono">UPDATES</h3>
+          {updates.map((x) => (
+            <pre
+              key={x.commit_timestamp}
+              className="text-xs overflow-scroll border border-black rounded-md m-2 p-2"
+              style={{ maxHeight: 200 }}
+            >
+              {JSON.stringify(x, null, 2)}
+            </pre>
+          ))}
+        </div>
+      </div>
+      <div className="col w-1/3">
+        <div>
+          <h3 className="font-mono">DELETES</h3>
+          {deletes.map((x) => (
+            <pre
+              key={x.commit_timestamp}
+              className="text-xs overflow-scroll border border-black rounded-md m-2 p-2"
+              style={{ maxHeight: 200 }}
+            >
+              {JSON.stringify(x, null, 2)}
+            </pre>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
