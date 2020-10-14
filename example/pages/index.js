c1d9c912cba33d784b14d793eb6317afa5e653e3
Paul Copplestone <pcopplestone@gmail.com>
2020-10-14 14:29:18 +0800

    Adds an example of how to unsubscribe and resubscribe

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,31 +1,24 @@
 import { useEffect, useState } from 'react'
+import { Socket } from '@supabase/realtime-js'
 import {
   NEXT_PUBLIC_SUPABASE_URL,
   NEXT_PUBLIC_SUPABASE_KEY,
 } from '../lib/constants'
 
-import { Socket } from '@supabase/realtime-js'
-
 var socket = new Socket(NEXT_PUBLIC_SUPABASE_URL, {
   params: { apikey: NEXT_PUBLIC_SUPABASE_KEY },
 })
 
-var publicSchema = socket.channel('realtime:public')
+var publicSchema = null
 
 export default function IndexPage() {
   let [inserts, setInserts] = useState([])
   let [updates, setUpdates] = useState([])
   let [deletes, setDeletes] = useState([])
   let [socketStatus, setSocketStatus] = useState('')
-  let [channelStatus, setChannelStatus] = useState(publicSchema.state)
-
-  
+  let [channelStatus, setChannelStatus] = useState('')
 
   useEffect(() => {
-    publicSchema.on('INSERT', (e) => setInserts([...inserts, e]))
-    publicSchema.on('UPDATE', (e) => setUpdates([...updates, e]))
-    publicSchema.on('DELETE', (e) => setDeletes([...deletes, e]))
-
     // Socket events
     socket.onOpen(() => setSocketStatus('OPEN'))
     socket.onClose(() => setSocketStatus('CLOSED'))
@@ -33,33 +26,56 @@ export default function IndexPage() {
       setSocketStatus('ERROR')
       console.log('Socket error', e.message)
     })
+    // Subscribe
+    createSubscription()
+
+    socket.connect()
+  }, [])
+
+  const createSubscription = () => {
+    publicSchema = socket.channel('realtime:public')
+    publicSchema.on('INSERT', (e) => setInserts([...inserts, e]))
+    publicSchema.on('UPDATE', (e) => setUpdates([...updates, e]))
+    publicSchema.on('DELETE', (e) => setDeletes([...deletes, e]))
 
     // Channel events
     publicSchema.onError(() => setChannelStatus('ERROR'))
     publicSchema.onClose(() => setChannelStatus('Closed gracefully.'))
     publicSchema
       .subscribe()
-      .receive('ok', () => setChannelStatus('CONNECTED'))
+      .receive('ok', () => setChannelStatus('SUBSCRIBED'))
       .receive('error', () => setChannelStatus('FAILED'))
       .receive('timeout', () => setChannelStatus('Timed out, retrying.'))
-  }, [])
+  }
 
-  socket.connect()
+  const toggleSubscription = () => {
+    if (channelStatus == 'SUBSCRIBED') {
+      publicSchema.unsubscribe()
+    } else {
+      createSubscription()
+    }
+  }
 
   return (
-    <div className="p-2">
+    <div className="p-2 font-mono">
       <div className="border-b py-8">
         <h4>SOCKET STATUS: {socketStatus}</h4>
         <h4>CHANNEL STATUS: {channelStatus}</h4>
+        <button
+          className="rounded border border-black p-2 text-sm my-2"
+          onClick={() => toggleSubscription()}
+        >
+          {channelStatus == 'SUBSCRIBED' ? 'Unsubscribe' : 'Subscribe'}
+        </button>
       </div>
-      <div className="w-full h-full flex py-8">
-        <div className="col w-1/3">
+      <div className="w-full h-full grid grid-cols-3 py-8 gap-2">
+        <div className="">
           <div>
-            <h3 className="font-mono">INSERTS</h3>
+            <h3 className="">INSERTS</h3>
             {inserts.map((x) => (
               <pre
                 key={x.commit_timestamp}
-                className="text-xs overflow-scroll border border-black rounded-md m-2 p-2"
+                className="text-xs overflow-scroll border border-black rounded-md my-2 p-2"
                 style={{ maxHeight: 200 }}
               >
                 {JSON.stringify(x, null, 2)}
@@ -67,13 +83,13 @@ export default function IndexPage() {
             ))}
           </div>
         </div>
-        <div className="col w-1/3">
+        <div className="">
           <div>
-            <h3 className="font-mono">UPDATES</h3>
+            <h3 className="">UPDATES</h3>
             {updates.map((x) => (
               <pre
                 key={x.commit_timestamp}
-                className="text-xs overflow-scroll border border-black rounded-md m-2 p-2"
+                className="text-xs overflow-scroll border border-black rounded-md my-2 p-2"
                 style={{ maxHeight: 200 }}
               >
                 {JSON.stringify(x, null, 2)}
@@ -81,13 +97,13 @@ export default function IndexPage() {
             ))}
           </div>
         </div>
-        <div className="col w-1/3">
+        <div className="">
           <div>
-            <h3 className="font-mono">DELETES</h3>
+            <h3 className="">DELETES</h3>
             {deletes.map((x) => (
               <pre
                 key={x.commit_timestamp}
-                className="text-xs overflow-scroll border border-black rounded-md m-2 p-2"
+                className="text-xs overflow-scroll border border-black rounded-md my-2 p-2"
                 style={{ maxHeight: 200 }}
               >
                 {JSON.stringify(x, null, 2)}
